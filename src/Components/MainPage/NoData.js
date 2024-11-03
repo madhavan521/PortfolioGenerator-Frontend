f2fbe4424fc801aa18e7fc6f79c43179afb96d28
Madhavan C <139116570+madhavan521@users.noreply.github.com>
2024-11-03 11:54:13 +0530

    NoData.js

--- src/Components/MainPage/NoData.js
+++ src/Components/MainPage/NoData.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import nodata from './InstructionAssest/NoData.jpg'
 
+
 const NoData = () => {
+
 
 const handleReload=()=>{
   window.location.reload()
@@ -10,9 +12,9 @@ const handleReload=()=>{
 
   return (
     <div >
-    <div className="row">
-    <p className="text-center">After Entering The Data Please Reload</p>
-      <button className='btn btn-danger'onClick={handleReload} style={{width:"150pc"}} >Reload</button>
+    <div className="row d-flex align-item-center text-center justify-content-center">
+    <p className="text-center h5">After Entering The Data Please Reload</p>
+      <button className='btn btn-danger d-flex align-item-center text-center justify-content-center'onClick={handleReload} style={{width:"150px"}} >Reload</button>
     </div>
       <img src={nodata} alt="" style={{width:"100%"}} />
     </div>
